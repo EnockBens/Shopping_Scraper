8331fde58ee454f81db3fd5e02f303b4fcef5155
Enock Bens <bensonkings81@gmail.com>
2024-08-01 18:20:12 +0300

    modified jiji_scrapper.py

--- scraper/jiji_scraper.py
+++ scraper/jiji_scraper.py
@@ -3,11 +3,10 @@ from bs4 import BeautifulSoup
 
 def scrape_jiji(query):
     search_url = f'https://www.jiji.co.ke/search?query={query}'
-    proxies = {
-        "http": None,
-        "https": None,
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
-    response = requests.get(search_url, proxies=proxies)
+    response = requests.get(search_url, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     products = []
@@ -19,7 +18,7 @@ def scrape_jiji(query):
         
         # Attempt to extract image URL
         img_tag = item.select_one('img')
-        if img_tag:
+        if (img_tag and 'src' in img_tag.attrs):
             image_url = img_tag['src']
         else:
             image_url = 'default_image_url'  # Use a placeholder if image URL is not found
